0dfe19f0b99411f9e1eb50464fcefd21694a3f4e
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2026-03-31 17:38:38 +0300

    added reverse proxy for render

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
+        destination: 'https://park-addis.onrender.com/api/:path*',
       },
     ];
   },
