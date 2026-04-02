7993506b6f68e26396a44b0cda367c94c34b8ea5
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2026-04-02 13:04:52 +0300

    used internal env(process) in chapa to test it

--- backend/src/chapa.ts
+++ backend/src/chapa.ts
@@ -2,7 +2,7 @@ import { Chapa } from "chapa-nodejs"
 import { env } from "./config/env"
 
 const chapa = new Chapa({
-    secretKey: env.CHAPA_SECRET_KEY
+    secretKey: `${process.env.CHAPA_SECRET_KEY}`
 })
 
 export default chapa
