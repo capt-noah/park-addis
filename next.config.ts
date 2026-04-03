caff11b70a8c25a8c1997b1a20714c02d3fa9e2b
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2026-04-03 09:30:29 +0300

    test1.3

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
+        destination: `${process.env.BACKEND_URL}:1000/api/:path*`,
       },
     ];
   },
